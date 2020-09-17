199a0f65d2ec2d00d9ab0abb27bc1c98c3abf71b
fanyCaz <stepheniecaz@gmail.com>
2020-09-16 23:19:16 -0500

    carpeta para problema mochila

---
+++ mochila/Mochila.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BoundedKnapsack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            
+        }
+    }
+}
