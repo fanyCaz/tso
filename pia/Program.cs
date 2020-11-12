a5e8ff799165a1eba5fdaf84274e9b08162bf0b8
Estefania Caceres <stepheniecaz@gmail.com>
2020-11-11 22:11:48 -0600

    variables

---
+++ pia/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace pia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize parameters
+            //F -> number of frogs
+            //m -> number of memeplexes, F is distributed in these memeplexes
+            //Each memeplex consists of n frogs that F = mn
+            //f(i) -> fitness
+            //n -> number of frogs in each memeplex
+            //q -> number of frogs in a submemeplex
+            //Px -> position of a frog
+            //PB -> best solution
+            //PW -> worst solution
+            //S  -> the updated step size of the frog-leaping
+            //Smax -> maximum step size allowed by a frog after being affected
+            //i -> number of iterations
+            int F, m, n, i;
+            //wth is U(i) ??
+
+
+        }
+    }
+}
